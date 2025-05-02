432b0feb1921e4c684a955943f823a4b6c663007
Nastya Humeniuk <perevalova43@icloud.com>
2025-05-02 13:56:45 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,49 @@
 'use strict';
 
-// write code here
+const tablet = document.querySelectorAll('.field');
+
+tablet.forEach((table) => {
+  const input = table.querySelector('input');
+  const nameCell = document.createElement('label');
+  const inputId = input.id;
+  const inputPlaceholder = input.name;
+
+  nameCell.textContent = input.name;
+  nameCell.setAttribute('for', inputId);
+  input.setAttribute('placeholder', inputPlaceholder);
+  nameCell.classList.add('field-label');
+  input.insertAdjacentElement('beforebegin', nameCell);
+});
+
+const loginForm = document.querySelectorAll('form')[1];
+
+loginForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const email = document.getElementById('sign-in-email').value.trim();
+  const password = document.getElementById('sign-in-password').value.trim();
+
+  if (email === '' || password === '') {
+    alert('Please fill in all fields');
+  } else {
+    alert('Login successfully');
+  }
+
+  const signUpForm = document.querySelectorAll('form')[0];
+
+  signUpForm.style.display = 'none';
+
+  const signInForm = document.querySelectorAll('form')[1];
+
+  signInForm.style.display = 'block';
+});
+
+window.addEventListener('load', function () {
+  const signUpForm = document.querySelectorAll('form')[0];
+
+  signUpForm.style.display = 'none';
+
+  const signInForm = document.querySelectorAll('form')[1];
+
+  signInForm.style.display = 'block';
+});
