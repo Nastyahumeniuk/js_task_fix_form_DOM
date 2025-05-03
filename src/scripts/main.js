8fe9b6d4e228f5c3abe8e0b5da7125283ba307eb
Nastya Humeniuk <perevalova43@icloud.com>
2025-05-03 10:47:56 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,53 +1,22 @@
 'use strict';
 
-const tablet = document.querySelectorAll('.field');
+'use strict';
 
-tablet.forEach((table) => {
-  const input = table.querySelector('input');
-  const nameCell = document.createElement('label');
-  const inputId = input.id;
-  const inputPlaceholder = input.name;
+'use strict';
 
-  nameCell.textContent = input.name;
-  nameCell.setAttribute('for', inputId);
-  input.setAttribute('placeholder', inputPlaceholder);
-  nameCell.classList.add('field-label');
-  input.insertAdjacentElement('beforebegin', nameCell);
-});
+const inputs = document.querySelectorAll('form input');
 
-const loginForm = document.querySelectorAll('form')[1];
+inputs.forEach((input) => {
+  const label = document.createElement('label');
 
-loginForm.addEventListener('submit', function (e) {
-  e.preventDefault();
+  label.textContent = input.name;
+  label.classList.add('field-label');
+  label.setAttribute('for', input.id);
 
-  const email = document.getElementById('sign-in-email').value.trim();
-  const password = document.getElementById('sign-in-password').value.trim();
+  input.setAttribute(
+    'placeholder',
+    input.name.charAt(0).toUpperCase() + input.name.slice(1),
+  );
 
-  if (email === '' || password === '') {
-    alert('Please fill in all fields');
-  } else {
-    document.querySelectorAll('form')[0].style.display = 'none';
-    document.querySelectorAll('form')[1].style.display = 'none';
-
-    const successMessage = document.createElement('div');
-
-    successMessage.textContent = 'Вітаємо! Ви успішно увійшли.';
-    successMessage.classList.add('success-message');
-    successMessage.style.fontSize = '24px';
-    successMessage.style.color = 'green';
-    successMessage.style.marginTop = '20px';
-    successMessage.style.textAlign = 'center';
-
-    document.body.appendChild(successMessage);
-  }
-});
-
-window.addEventListener('load', function () {
-  const signUpForm = document.querySelectorAll('form')[0];
-
-  signUpForm.style.display = 'none';
-
-  const signInForm = document.querySelectorAll('form')[1];
-
-  signInForm.style.display = 'block';
+  input.insertAdjacentElement('beforebegin', label);
 });
